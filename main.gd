b75c22cc7c7a89662ac47b005bdbe25d49064b15
moritz-prenner <moritz.prenner@icloud.com>
2025-07-23 15:30:26 +0200

    new map

---
+++ main.gd
@@ -0,0 +1,10 @@
+extends Node
+
+@onready var anim = $Player/AnimatedSprite2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	anim.play("idle")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
