3cd95cf6419c924bbd1fe418e98529831089b91f
moritz-prenner <moritz.prenner@icloud.com>
2025-07-24 18:45:03 +0200

    fixed bugs

--- elevator.gd
+++ elevator.gd
@@ -5,7 +5,6 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	anim.play("default")
-	print("started")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,6 +14,7 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body == player:
+		print("touched")
 		body.elevate()
 	
 
